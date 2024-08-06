2019e3b65e7aca403cead1290cb3dd3237c4b69d
Allan Silva <allan@allansilva.com.br>
2024-08-06 09:14:30 -0300

    Fix sintax

--- book/c2/rocket_data_pipeline.py
+++ book/c2/rocket_data_pipeline.py
@@ -19,7 +19,7 @@ dag = DAG(
 
 download_launches = BashOperator(
     task_id="download_launches",
-    bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'"
+    bash_command="curl -o /tmp/launches.json -L 'https://ll.thespacedevs.com/2.0.0/launch/upcoming'",
     dag=dag
 )
 
