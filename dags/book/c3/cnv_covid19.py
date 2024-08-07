f9cb2d6af3caef4b6c3ad7df2421f5ec6841d0ad
Allan Silva <allan@allansilva.com.br>
2024-08-07 08:25:03 -0300

    Bootstrapping Ministerio da Saude - Covid19 - CNV

--- dags/book/c3/cnv_covid19.py
+++ dags/book/c3/cnv_covid19.py
@@ -1,1 +1,33 @@
+import airflow
+
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+
 from dags.commons.ms import MSElasticsearch
+
+ms_elasticsearch = MSElasticsearch()
+
+
+dag = DAG(
+    dag_id="cnv_covid19",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None
+)
+
+
+fetch_vaccination_data = PythonOperator(
+    task_id="fetch_vaccination_data",
+    python_callable=ms_elasticsearch.dag_get,
+    dag=dag
+)
+
+
+notify = PythonOperator(
+    task_id="notify_complete",
+    python_callable=lambda: print("Finished execution!")
+)
+
+fetch_vaccination_data >> notify
--- dags/commons/ms.py
+++ dags/commons/ms.py
@@ -1,2 +1,9 @@
 class MSElasticsearch:
-    pass
+    def dag_get(self, ts):
+        self.get(ts)
+
+    def get(self, fetch_day):
+        print("*" * 100)
+        print("Fetching data from MS Elasticsearch.")
+        print(fetch_day)
+        print("*" * 100)
