88c670d7fdd97a51ec6b4f87b1e750e79122752f
Allan Silva <allan@allansilva.com.br>
2024-08-10 11:01:00 -0300

    Improve Elasticsearch return

--- labs/commons/ms.py
+++ labs/commons/ms.py
@@ -1,14 +1,43 @@
 import pendulum
+import os
+import json
+import requests
+
+from dataclasses import dataclass
+
+from requests.auth import HTTPBasicAuth
 
 from labs.commons.dates import DEFAULT_TIME_ZONE, DEFAULT_FORMAT
 
 
+@dataclass
+class ElasticOp:
+    request_payload: str
+    response: dict
+    extra_data: dict
+
+
 class MSElasticsearch:
+    def __init__(self):
+        self.cluster_url = os.getenv("MS_COVID19_CNV_CLUSTER_URL")
+        self.index = "desc-imunizacao"
+        self.search_api_endpoint = "_search"
+        self.user = os.getenv("MS_COVID19_CNV_CLUSTER_USER")
+        self.password = os.getenv("MS_COVID19_CNV_CLUSTER_PASS")
+
+    @property
+    def search_url(self):
+        return f"{self.cluster_url}/{self.index}/{self.search_api_endpoint}"
+
+    @property
+    def auth(self):
+        return HTTPBasicAuth(self.user, self.password)
+
     def dag_get(self, ts):
         self.get_daily(ts)
 
     def get_daily(self, fetch_day):
-        
+
         day = pendulum.parse(fetch_day, tz=DEFAULT_TIME_ZONE)
         start = day.start_of("day").format(DEFAULT_FORMAT)
         end = day.end_of("day").format(DEFAULT_FORMAT)
@@ -26,12 +55,7 @@ class MSElasticsearch:
             "size": 100,
         }
 
-        print("*" * 100)
-        print("Fetching data from MS Elasticsearch.")
-        print(DEFAULT_TIME_ZONE)
-        print(start)
-        print(end)
-        print(payload)
-        print("*" * 100)
+        response = requests.get(self.search_url, auth=self.auth, json=payload)
+        response.raise_for_status()
 
-        return payload, start, end
+        return ElasticOp(payload, response.json(), {"start": start, "end": end})
--- labs/tests/ministerio_saude_covid19_campanha_vacinacao_integration_test.py
+++ labs/tests/ministerio_saude_covid19_campanha_vacinacao_integration_test.py
@@ -1,19 +1,28 @@
-import pendulum
+import os
+import pytest
 
 from labs.commons.ms import MSElasticsearch
 
 
-def test_get_daily():
+@pytest.fixture
+def env_vars():
+    os.environ["MS_COVID19_CNV_CLUSTER_URL"] = "https://imunizacao-es.saude.gov.br"
+    os.environ["MS_COVID19_CNV_CLUSTER_USER"] = "imunizacao_public"
+    os.environ["MS_COVID19_CNV_CLUSTER_PASS"] = "qlto5t&7r_@+#Tlstigi"
+
+
+def test_get_daily(env_vars):
 
     string_date = "2024-08-07T13:19:09.274542+00:00"
     expected_string_start_date = "2024-08-07T00:00:00.000+00:00"
     expected_string_end_date = "2024-08-07T23:59:59.999+00:00"
 
     ms_elastic = MSElasticsearch()
-    payload, start_date, end_date = ms_elastic.get_daily(string_date)
+    elastic_op = ms_elastic.get_daily(string_date)
 
     print(expected_string_start_date)
-    assert expected_string_start_date == start_date
-    assert expected_string_end_date == end_date
-    assert expected_string_start_date == payload["query"]["range"]["@timestamp"]["gte"]
-    assert expected_string_end_date == payload["query"]["range"]["@timestamp"]["lte"]
+    assert expected_string_start_date == elastic_op.request_payload["query"]["range"]["@timestamp"]["gte"]
+    assert expected_string_end_date == elastic_op.request_payload["query"]["range"]["@timestamp"]["lte"]
+    assert expected_string_start_date == elastic_op.extra_data["start"]
+    assert expected_string_end_date == elastic_op.extra_data["end"]
+    
