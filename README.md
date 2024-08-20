c269912a0fe155733a64db612cd33fb51505bd17
Allan Silva <allan@allansilva.com.br>
2024-08-20 04:35:13 -0300

    book c4

--- README.md
+++ README.md
@@ -3,8 +3,14 @@ My labs - Airflow DAGs
 
 ## Misc
 
-Render template args without execute task:
+### Render template args without execute task:
 
 `airflow tasks render [dag id] [task id] [desired execution date]`  
 
 `airflow tasks render cnv_covid19 fetch_vaccination_data 2024-08-07T00:00:00+00:00`
+
+
+### Add Airflow connection:
+
+`airflow connections add --conn-type postgres --conn-host 192.168.60.30 --conn-login labsu --conn-password labsu postgres-default`
+
--- labs/book/c4/wiki_stock_sense.py
+++ labs/book/c4/wiki_stock_sense.py
@@ -4,12 +4,14 @@ import pendulum
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 
 dag = DAG(
     dag_id="wiki_stock_sense",
     start_date=pendulum.now().subtract(days=1),
     schedule_interval="@hourly",
+    template_searchpath="/tmp",
 )
 
 
@@ -30,7 +32,7 @@ get_data = PythonOperator(
     python_callable=_get_data,
     op_kwargs={
         "wiki_url": "https://dumps.wikimedia.org/other/pageviews",
-        "output_path": "/tmp/wikipageviews.gz"
+        "output_path": "/tmp/wikipageviews.gz",
     },
     dag=dag,
 )
@@ -44,28 +46,36 @@ extract_data = BashOperator(
 
 
 def _fetch_pageviews(pagenames, execution_date, **_):
-   result = dict.fromkeys(pagenames, 0)
-   with open("/tmp/wikipageviews", "r") as f:
-       for line in f:
-           domain_code, page_title, view_counts, _ = line.split(" ")
-           if domain_code == "en" and page_title in pagenames:
-               result[page_title] = view_counts
- 
-   with open("/tmp/postgres_query.sql", "w") as f:
-       for pagename, pageviewcount in result.items():
-           f.write(
-               "INSERT INTO pageview_counts VALUES ("
-               f"'{pagename}', {pageviewcount}, '{execution_date}'"
-               ");\n"
-           )
- 
- 
+    result = dict.fromkeys(pagenames, 0)
+    with open("/tmp/wikipageviews", "r") as f:
+        for line in f:
+            domain_code, page_title, view_counts, _ = line.split(" ")
+            if domain_code == "en" and page_title in pagenames:
+                result[page_title] = view_counts
+
+    with open("/tmp/postgres_query.sql", "w") as f:
+        for pagename, pageviewcount in result.items():
+            f.write(
+                "INSERT INTO pageview_counts VALUES ("
+                f"'{pagename}', {pageviewcount}, '{execution_date}'"
+                ");\n"
+            )
+
+
 fetch_pageviews = PythonOperator(
-   task_id="fetch_pageviews",
-   python_callable=_fetch_pageviews,
-   op_kwargs={"pagenames": {"Google", "Amazon", "Apple", "Microsoft", "Facebook"}},
-   dag=dag,
+    task_id="fetch_pageviews",
+    python_callable=_fetch_pageviews,
+    op_kwargs={"pagenames": {"Google", "Amazon", "Apple", "Microsoft", "Facebook"}},
+    dag=dag,
 )
 
 
-get_data >> extract_data >> fetch_pageviews
+write_to_postgres = PostgresOperator(
+    task_id="write_to_postgres",
+    postgres_conn_id="postgres-default",
+    sql="postgres_query.sql",
+    dag=dag,
+)
+
+
+get_data >> extract_data >> fetch_pageviews >> write_to_postgres
